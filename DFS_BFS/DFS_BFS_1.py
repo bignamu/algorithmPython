d3d1b54e1ac6e55ca667065e54c9c480083487fe
hodlMaster <sunohare7@gmail.com>
2021-07-04 10:20:41 +0900

    main

--- DFS_BFS/DFS_BFS_1.py
+++ DFS_BFS/DFS_BFS_1.py
@@ -1,14 +1,25 @@
+from itertools import combinations
+
 def solution(numbers, target):
     answer = 0
+    diff =  target - sum(numbers)
+    comstk = 0
     
-    print(sum(numbers))
+    for i in range(1,len(numbers)):
+        comb = list(combinations(numbers,i))
+        for j in range(len(comb)):
+            if sum(list(comb[j]))*(-2) == diff:
+                comstk += 1
+
     
+
     
+    answer = comstk
     return answer
 
 
 
-numbers = [1, 1, 1, 1, 1] # 5
+numbers = [1, 2, 1, 1, 1] # 5
 target = 3	
 
 solution(numbers,target)
