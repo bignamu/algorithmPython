65c77d73898472f7a2b15d84d91407d74f841caa
hodlMaster <sunohare7@gmail.com>
2021-07-09 00:07:14 +0900

    main

--- test.py
+++
@@ -1,4 +0,0 @@
-li = [[1]]
-
-
-print(li==True,li==False,1==False)
--- 진짜테스트용.py
+++ 진짜테스트용.py
@@ -1,1 +1,11 @@
 print([0]*5)
+
+
+s1 = 'hht'
+s2 = 'hit'
+print(list(set(s1)-set(s2)))
+print(list(set(s2)-set(s1)))
+
+s1list = [s11 for s11 in s1 if s11 in s2]
+
+print(s1list)
