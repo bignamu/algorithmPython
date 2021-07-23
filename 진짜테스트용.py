72ceef6690a2c78caa4dd041493faa90f8b042e2
hodlMaster <sunohare7@gmail.com>
2021-07-23 12:02:11 +0900

    main

--- 진짜테스트용.py
+++ 진짜테스트용.py
@@ -1,7 +1,16 @@
 import sys
+import re
+
+
+str1 = 'ABCA'
+str2 = 'ABABCA'
+
+for i in list(str1):
+    print(re.findall(i,str2))
 
 
 
-s = 'K123'
 
-print(s[-3:])
+_list = [1,'abc','b']
+
+print(_list['abc'])
