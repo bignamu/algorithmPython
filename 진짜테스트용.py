bedce110d0f49fbc023400195a00af3e783d2989
hodlMaster <sunohare7@gmail.com>
2021-08-16 21:46:37 +0900

    main

--- 진짜테스트용.py
+++ 진짜테스트용.py
@@ -1,48 +1,5 @@
 
-import os
-import argparse
+import math
 
-
-print(os.path.dirname('H:\Resources\Algorithm\Python\kakaopay04-4.png').split('\\')[-1])
-
-print(min(1,0))
-
-p = 'is'
-t = 'This is a book~!'
-
-M = len(p)
-N = len(t)
-
-def BruteForce(p,t):
-    i = 0
-    j = 0
-    while j < M and i < N:
-        if t[i] != p[j]:
-            i= i - j
-            j = -1
-
-        i += 1
-        j += 1
-    if j == M:
-        return i-M
-    else:return -1
-
-
-print(BruteForce(p,t))
-
-a = type('abc')
-print(a.__name__)
-
-
-
-
-A_list = ['a', 'b', 'c'] 
-B_list = ['d', 'e', 'c', 'd', 'e', 'f']
-
-clist = A_list+B_list
-print('C_List',clist)
-
-A_set = set(A_list)
-B_set = set(B_list)
-C_set = A_set | B_set
-print('C_set',C_set)
+def ddalbae(price):
+    
