9284594c546a1101d2a2750c97ce8155c5848ddb
hodlMaster <sunohare7@gmail.com>
2021-09-09 00:27:21 +0900

    main

--- DFS_BFS/DFS_BFS_4.py
+++ DFS_BFS/DFS_BFS_4.py
@@ -2,34 +2,50 @@
 
 # 테케 12 실패
 from collections import defaultdict
+import copy
 
 
 def solution(tickets):
+    
+    
+    global _dict, visited,answer
     answer = []
     _dict = defaultdict(list)
-    visited = {}
+    visited = defaultdict(int)
     
     for tic in tickets:
         start, end = tic
         _dict[start].append(end)
-        visited[start+end] = False
+        visited[start+end] += 1
         
     
-    _list = list(visited.keys())
+    visited2 = copy.deepcopy(visited)
+    _dict2 = copy.deepcopy(_dict)
+    print(visited,_dict)
 
-    # print(visited,_dict)
+    global previous   
+    previous = "ICN"
     
     def dfs(start):
-        
-        # print(f'!!now {start}')
+        global _dict
+        global visited, answer
+        if len(answer) < len(tickets)+1 and not _dict[start]:
+            visited = copy.deepcopy(visited2)
+            _dict = copy.deepcopy(_dict2)
+            answer = []
+            return
+
+        print(f'!!now {start}')
         answer.append(start)
-        graph = _dict[start]    
+        graph = _dict[start]
         graph.sort()
+
+
         for v in graph:
-            # print(f'check {v}')
-            if not visited[start+v]:
-                visited[start+v] = True
-                
+            print(f'check {v}')
+            if visited[start+v]:
+                visited[start+v] -= 1
+                _dict[start].remove(v)
                 
                 dfs(v)
     
@@ -38,6 +54,6 @@ def solution(tickets):
     return answer
 
 
-tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+tickets = [['ICN','B'],['B','ICN'],['ICN','A'],['A','D'],['D','A']]
 
 print(solution(tickets))
